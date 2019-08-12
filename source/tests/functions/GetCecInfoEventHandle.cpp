411fbca825fb49d22e2a2e75c484dc9601f3f845
NarcolepticK <NarcolepticKrias@gmail.com>
2019-08-12 19:09:12 -0400

    tests/GetCecInfoEventHandle: add test

--- source/tests/functions/GetCecInfoEventHandle.cpp
+++ source/tests/functions/GetCecInfoEventHandle.cpp
@@ -17,6 +17,14 @@ GetCecInfoEventHandle::~GetCecInfoEventHandle() {
 
 void GetCecInfoEventHandle::RunTest() {
     log << "Starting Test" << std::endl;
+
+    Result res;
+    Handle handle = 0;
+    
+    res = CECDU_GetCecInfoEventHandle(&handle);
+    log << "  result: " << std::hex << res << " handle: " << handle << std::endl;
+
+
     log << "Test Finished" << std::endl;
 }
 
